e89a47c91bb19f660d8c46d8431a3079aeb4d970
leechardes <leechardes@gmail.com>
2024-10-08 10:38:22 -0300

    Controle de versão

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from config.settings import set_page_config
 from views.config import show_config
 from utils.helpers import apply_filters
 
+def create_version_page():
+    st.title("Versão Atual")
+    st.write("Número da versão: 1.0.0")
+    st.write("Data de lançamento: 08/10/2024")
+    st.write("Alterações:")
+    st.write("* Correção de bugs")
+    st.write("* Adição de recursos novos")
+
+    # st.title("Histórico de Versões")
+    # st.write("Versão 0.9.0: 08/10/2024")
+    # st.write("* Correção de bugs")
+    # st.write("* Melhoria de desempenho")
+
+    st.title("Notas de Lançamento")
+    st.write("Versão 1.0.0: 08/10/2024")
+    st.write("* Adição de recursos novos")
+    st.write("* Correção de bugs")
+
 def main():
     set_page_config()
     df = load_data()
@@ -19,6 +37,8 @@ def main():
         show_overview(df_filtered, exibe_grafico, exibe_por)
     elif page == "Configuração":
         show_config()
+    elif page == "Versão":
+        create_version_page()
     else:
         st.write("Página em desenvolvimento")
 
