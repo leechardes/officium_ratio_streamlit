b5302856044d142ea8ffebccb907ab1b4c538f24
leechardes <43678643+leechardes@users.noreply.github.com>
2024-10-30 20:59:17 +0000

    Atualização de versão

--- views/version.py
+++ views/version.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def show_version():
     st.title("Versão Atual")
-    st.write("Número da versão: 1.0.2")
-    st.write("Data de lançamento: 08/10/2024")
+    st.write("Número da versão: 1.0.3")
+    st.write("Data de lançamento: 30/10/2024")
     st.write("Alterações:")
     st.write("* Correção de bugs")
     st.write("* Adição de recursos novos")
@@ -17,6 +17,10 @@ def show_version():
     st.write("Versão 1.0.2: 08/10/2024")
     st.write("* Upload e processamento de arquivos CSV")
 
+    st.title("Histórico de Versões")
+    st.write("Versão 1.0.3: 30/10/2024")
+    st.write("* Ajuste nos calculos e utilização do banco de dados mongodb")
+
     st.title("Notas de Lançamento")
-    st.write("Versão 1.0.2: 08/10/2024")
+    st.write("Versão 1.0.3: 08/10/2024")
     st.write("* Melhorias..")
